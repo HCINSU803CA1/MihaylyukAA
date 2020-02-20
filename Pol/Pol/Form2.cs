9ab6ee020214ef5c1c52078dabbf3738610dba87
alexmihalyk23 <35634279+alexmihalyk23@users.noreply.github.com>
2020-02-20 19:30:30 +0700

    Update Form2.cs

--- Pol/Pol/Form2.cs
+++ Pol/Pol/Form2.cs
@@ -22,22 +22,7 @@ namespace Pol
         private char GetSign(char character)
         {
 
-                if (character == '+')
-                {
-                    return '+';
-                }
-                if (character == '*')
-                {
-                    return '*';
-                }
-                if (character == '/')
-                {
-                    return '/';
-                }
-                if (character == '-')
-                {
-                    return '-';
-                }
+                
 
             return character;
         }
@@ -45,7 +30,7 @@ namespace Pol
         private void TextBox1_TextChanged(object sender, EventArgs e)
         {
             string first = textBox1.Text;
-            char returnedSign = ' ';
+            char ch = ' ';
 
             if ((!textBox1.Text.Equals("")) && (!textBox1.Text.EndsWith("=")))
             {
@@ -56,45 +41,87 @@ namespace Pol
                 
                 foreach (var sign in s)
                 {
-                   returnedSign = GetSign(sign);
-                }
-
-                var stringArray = s.Split(new char[] { '+', '-', '*', '/'});
-               // Console.WriteLine(returnedSign);
-                
-                foreach (var text in stringArray)
-                {
-                    if (!text.Equals(""))
+                    
+                    if (sign == '+')
                     {
-                        
-                        var number = int.Parse(text);
-                        Console.WriteLine(number);
-
-                        //Array.Append(number);
-                        if (Array[0] == 0)
+                        ch = '+';
+                    }
+                    if (sign == '*')
+                    {
+                        ch ='*';
+                    }
+                    if (sign == '/')
+                    {
+                        ch= '/';
+                    }
+                    if (sign == '-')
+                    {
+                        ch = '-';
+                    }
+                    if (sign == '=')
+                    {
+                        ch = '=';
+                    }
+                    var stringArray = s.Split(new char[] { '+', '-', '*', '/' });
+                    foreach (var text in stringArray)
+                    {
+                        if (!text.Equals(""))
                         {
-                            Array[0] = number;
-                        }
-                        else
-                        {
-                            Array[1] = number;
-                        }
+                            Console.WriteLine("ret" + ch);
+                            var number = int.Parse(text);
+                            Console.WriteLine(number);
 
-                        Console.WriteLine(Array[0]);
+                            //Array.Append(number);
+                            if (Array[0] == 0)
+                            {
+                                Array[0] = number;
+                            }
+                            else
+                            {
+                                Array[1] = number;
+                            }
+                        }
+                        Console.WriteLine("ret" + ch);
+                        label1.Text = "0";
+                        if (ch == '+')
                             
-                        
+                        {
+
+                            double Result = Array[0] + Array[1];
+                            label1.Text = (Convert.ToString(Result));
+
+                            
+                        }
+                        if (ch == '-')
+                        {
+
+                            double Result = Array[0] - Array[1];
+                            label1.Text = (Convert.ToString(Result));
+
+
+                        }
+                        if (ch == '*')
+                        {
+
+                            double Result = Array[0] * Array[1];
+                            label1.Text = (Convert.ToString(Result));
+
+
+                        }
+                        if (ch == '/')
+                        {
+
+                            double Result = Array[0] / Array[1];
+                            label1.Text = (Convert.ToString(Result));
+
+
+                        }
+                           
                     }
                 }
             }
-                 Console.WriteLine("ret"+ returnedSign);
-                if (first.EndsWith("="))
-                {
-                   
-                    double Result = Array[0] - Array[1];
-                    label1.Text = (Convert.ToString(Result));
-
-                    //Console.WriteLine(Array[0] + Array[1]);
-                }
+                 
+                
             
             
         }
