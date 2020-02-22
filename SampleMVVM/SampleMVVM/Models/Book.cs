37b82824ca5affb995427565984dd53267d8d7c1
Alexey Mikhailyuk <35634279+alexmihalyk23@users.noreply.github.com>
2020-02-22 12:10:09 +0700

    Lab 3
    Лаба 3 це пиз**ец

---
+++ SampleMVVM/SampleMVVM/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace SampleMVVM.Models
+{
+    class Book
+    {
+        public string OutputText { get; set; }
+        public string Author { get; set; }
+        public int Count { get; set; }
+
+        public Book(string title, string author, int count)
+        {
+            this.OutputText = title;
+            this.Author = author;
+            this.Count = count;
+        }
+
+    }
+}
